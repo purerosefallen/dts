96fd44bcf9e6a2fd334426a1678516efd2124a1a
purerosefallen <78877@qq.com>
2019-06-30 21:53:46 +0800

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-if [[ ! -f "./game.php" ]]; then
-	cp -rf /defaults/* .
+if [[ ! -f "./game.php" ]]; then # first run
+	cp -rf /defaults/* .;
+	chmod -R 777 ./gamedata;
+	chmod -R 777 ./include;
+	chmod -R 777 ./templates;
 fi
 ./acdts-daemonctl.sh &
 apache2-foreground
